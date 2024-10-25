1d7c042547bb0ffd3d3984525de0e36d66b25b8e
Beknur <106554639+BEknUur@users.noreply.github.com>
2024-10-25 22:26:27 +0500

    first commit

---
+++ lesson-1/example.py
@@ -0,0 +1,36 @@
+
+#  INPUT -енгізу операторы 
+'''
+output - шығару print()
+
+3 тип данных
+String -str  "Elzhan"
+char  'A'
+int  10
+double or float  12.23243
+'''
+
+#1
+'''
+x=140
+y="Sigma"
+print(x)
+print(type(x))
+print(type(y))
+'''
+#2
+san1=10
+san2=3
+print(san1%san2)# калдык болиги
+print(san1/san2)  # бутин болиги
+
+
+#3
+print("\n") # пробел 
+a=1.99
+b=int(a) # бір типтен екінші типке ауыстыру
+print(a)
+print(b)
+
+
+
